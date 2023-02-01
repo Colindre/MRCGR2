d4bb698accdf466945f4a2930a2351472772f862
LOUNICI <28600825@ppti-14-406-06.ufr-info-p6.jussieu.fr>
2023-02-01 09:30:45 +0100

    ajout de rayon et changement detecter

--- src/envSimu/robot.py
+++ src/envSimu/robot.py
@@ -1,76 +1,110 @@
+import math
+
 class Robot:
-	def __init__(self,posx, posy, dirr):
+	def __init__(self,posx, posy, dirr,rayon):
 		self.posx = posx					#position du robot sur l'axe des abscisse 
 		self.posy = posy					#position du robot sur l'axe des abscisse 
-		self.dirr  = dirr					#direction du robot (1 pour haut, 2 pour droite, 3 pour bas, 4 pour gauche)
-	
+		self.dirr  = dirr%360				#direction du robot (angle en degre)
+		self.rayon=rayon
+		
+			
 	def getPos(self):						#retourne la position
 		return (self.posx, self.posy)
 		
 	def getDirr(self):						#retourne la direction
 		return self.dirr
 		
-	def avancer(self):                                      	#avance le robot de 1 (en fonction de sa direction) 
-									#en prenant en compte que le point en bas a gauche est (0,0)
-		if(self.dirr==1):
-			self.posy +=1
-		elif(self.dirr==2):
-			self.posx +=1
+	def getRayon(self):
+		return self.rayon
 
-		elif(self.dirr==3):
-			self.posy -=1
+	def deplacement(self, vecteur):		#change direction du robot et le fait avancer, en prenant en compte que le point en bas a gauche est (0,0)
 
-		else:
-			self.posx -=1
-	
-	def reculer(self):						#recule le robot (similairement a avancer)
-
-		if(self.dirr==1):
-			self.posy -=1
-
-		elif(self.dirr==2):
-			self.posx -=1
-
-		elif(self.dirr==3):
-			self.posy +=1
-
-		else:
-			self.posx +=1
-	
-	def tourner(self, direction):					#tourne le robot selon une direction
-			if (direction == "d"):                                  	#cas "d" pour droite 
-					self.dirr = (self.dirr%4) + 1
-			elif (direction == "g"):                                	#cas "g" pour gauche
-				if (self.dirr == 1): 
-					self.dirr=4
-				else:
-					self.dirr -= 1
- 		
-            
-			else:                                                   	#cas argument non valide
-				print("Mauvais argument, le robot n'a pas tourne. Les parametres possibles sont: 'd' ou 'g'")
-			
-	
-	def detecter(self, tabPoints):					#verifie si le robot detecte un objet devant lui. tabPoints est un tableau de tuple
+		angle = angleVecteur(vecteur)			#calcul la nouvelle direction du robot et le fait tourner en celle-ci
+		self.rotation(angle - self.dirr)
+												#effectu le deplacement
+		vectX, vectY = vecteur
+		self.posx = (self.posx + vectX)
+		self.posy = (self.posy + vectY)
+		
+	def ensPointsRobots(self):
+		ens=set()
+		for i in range(self.posx, self.posx + self.rayon + 1):
+			for j in range(self.posy, self.posy + self.rayon + 1):
+				ens.add((i,j))
+		ensN=set()
+		for k in range(self.posx - self.rayon + 1,self.posx):
+			for l in range(self.posy - self.rayon + 1,self.posy):
+				ensN.add((k,l))
+		return ens.union(ensN)
+				
+	def detecter(self, tabPoints, ensPointsRobots, vecteur):					#verifie si le robot detecte un objet devant lui. tabPoints est un tableau de tuple
 	                                                        	#regarde si position du robot est dans le tableau de points
 		for p in tabPoints: 
-			if (p == self.getPos()):
-				print("Robot est sur un obstacle. Suite des operations impossible")
-				return True
-			
-			
-		if(self.dirr==1):										#regarder direction du robot et prend la valeur de la position apres un avancement (l'avancement n'est pas realise)
-			testPos = (self.posx, self.posy+1)
-		elif(self.dirr==2):
-			testPos = (self.posx+1, self.posy)
-		elif(self.dirr==3):
-			testPos = (self.posx, self.posy -1)
-		else:
-			testPos = (self.posx-1, self.posy)
+			for q in ensPointsRobots:
+				if (p == q):
+					print("Robot est sur un obstacle. Suite des operations impossible")
+					return True
+		self.deplacement(vecteur)
+		newEnsPoints=set()
+		newEnsPoints=self.ensPointsRobots()
 		
-		for p in tabPoints:										#regarde si la position est dans le tableau de points
-			if (p == testPos):
-				print("Obstacle devant le robot.")
-				return True
+		for p in tabPoints: 
+			for q in ensPointsRobots:
+				if (p == q):
+					print("Obstacle devant le robot.")
+					return True
 		print("pas d'obstacle devant")
 		return False
+
+	def rotation(self, angle):						#fait tourner le robot (angle positif pour tourner a gauche, negatif pour a droite)
+		angle     = math.radians(angle%360)
+		cos       = math.cos(angle) 
+		sin       = math.sin(angle)
+		direction = math.radians(self.dirr)
+		x, y      = math.cos(direction), math.sin(direction) 
+		x         = x*cos-y*sin
+		y         = x*sin+y*cos
+		print(angle+self.dirr)
+		print(math.degrees(math.acos(x)))
+		if(((math.degrees(angle)+self.dirr)%360 > 180)):
+			print("a")
+			self.dirr = 360 - math.degrees(math.acos(x))
+		elif((math.degrees(angle)+self.dirr)%360 < (-180)):
+			print("b")
+			self.dirr = 360 - math.degrees(math.acos(x))
+		else:
+			self.dirr = math.degrees(math.acos(x))
+
+def angleVecteur(vecteur):							#calcul l'angle positif du vecteur (par rapport a l'axe des abscisse)
+	x1, y1     = vecteur
+	x2, y2     = 1, 0
+	norme1     = math.sqrt(x1**2 + y1**2)
+	norme2     = math.sqrt(x2**2 + y2**2)
+	scalaire   = x1*x2 + y1*y2
+	angle      = math.degrees(math.acos(scalaire / (norme1*norme2)))
+	if(y1<0):										#permet de calculer l'angle positif
+		return 360-angle
+	else:
+		return angle
+
+
+
+#TEST 
+robot = Robot(2,2,90,3)
+ensPtsRbt= robot.ensPointsRobots()
+print("robot de rayon",robot.getRayon())
+print("le robot est sur les points:",ensPtsRbt)
+ensPtsObs={(6,6)}
+
+robot.detecter(ensPtsRbt,ensPtsObs,(1,1))
+
+
+print("--------------------------------")
+
+print("direction robot: ",robot.getDirr())
+print("position robot: ",robot.getPos())
+robot.deplacement((1,1))
+print("angle du vecteur: ",angleVecteur((1,1)))
+print("apres deplacement:")
+print("dir: ",robot.getDirr())
+print("pos: ",robot.getPos())
