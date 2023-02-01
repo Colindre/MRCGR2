17b553227ff27c734ec13491e6ce5acea5fc18d7
RODRIGO <21107747@ppti-14-406-05.ufr-info-p6.jussieu.fr>
2023-02-01 09:02:22 +0100

    ajout des test

--- src/envSimu/environnement.py
+++ src/envSimu/environnement.py
@@ -6,11 +6,6 @@ class Environnement:
 
     def getPos(self):
         return (self.max_x, self.max_y)
-
-
-    def afficherEnv(self):
-        rx , ry = self.getPos()
-        env[rx][ry]
     
     def add(self,robot):
         rx , ry = robot.getPos()
@@ -19,17 +14,17 @@ class Environnement:
             return
             
             
-        print("Le robot a été placé dans le monde en ",robot.getPos()," et est dirigé vers ",robot.getDirr())
+        print("Le robot a ete place dans le monde en ",robot.getPos()," et est dirige vers ",robot.getDirr())
 
     def addObstacle(self,obstacle):
         ox , oy = obstacle.getPos()
-        if((ox < 0) or (ox > self.max_x) or (0y < 0) or (oy > self.max_y)):
+        if((ox < 0) or (ox > self.max_x) or (oy < 0) or (oy > self.max_y)):
             print("Erreur : Les positions de l'obstacle sont en dehors du monde. Il n'a pas pu etre mis place")
             return
           
-        for l in listeObstacle:
+        for l in self.listeObstacle:
             if((ox,oy) == l):
-                print("Erreur : il y a déjà un obstacle à cette position. Il n'a pas pu etre mis en place")
+                print("Erreur : il y a deja un obstacle a cette position. Il n'a pas pu etre mis en place")
                 return
         
         self.listeObstacle.append((ox,oy))
@@ -40,6 +35,30 @@ class Environnement:
         if((rx < 0) or (rx > self.max_x) or (ry < 0) or (ry > self.max_y)):
             print("Erreur : Le robot se trouve en dehors du monde")
             return
-        print("Le robot se trouve désormais en ",robot.getPos()," et est dirigé vers ",robot.getDirr())
+        print("Le robot se trouve desormais en ",robot.getPos()," et est dirige vers ",robot.getDirr())
         
         
+class Obstacle:
+    def __init__(self,posx, posy, tailleX, tailleY):
+        self.posx     = posx                    #position du point le plus en bas a gauche du rectangle en abscisse 
+        self.posy     = posy                    #position du point le plus en bas a gauche du rectangle en ordonne
+        self.tailleX = tailleX                    #taille du rectangle en abscisse
+        self.tailleY  = tailleY                    #taille du rectangle en ordonne
+	
+    def getPos(self):                            #retourne la position du rectangle (le point le plus en bas a gauche)
+        return (self.posx, self.posy)
+
+    def getDimensions(self):                    #retourne les dimensions du rectangle (tailleX, tailleY)
+        return self.tailleX, self.tailleY
+        
+
+    def listePoints(self):                      #retourne la liste des points qu'occupe l'obstacle (innacessibles aux robots)
+        liste=[]
+        for i in range(self.posx, self.posx + self.tailleX + 1):
+            for j in range(self.posy, self.posy + self.tailleY + 1):
+                liste.append((i,j))
+        return liste
+
+#TEST
+o = Obstacle(0,0,2,4)
+print(o.listePoints())
---
+++ src/envSimu/test.py
@@ -0,0 +1,66 @@
+from robot import Robot, angleVecteur
+from environnement import Environnement, Obstacle
+
+
+#CREATION OBJET OBSTACLE ENVIRONNEMENT (100,100) ET ROBOT (position 0,0 et regarde vers le haut)
+print("\n------------CREATION OBJETS------------")
+robot1 = Robot(0,0,1)
+robot2 = Robot(96,13,1)
+o = Obstacle(3,4,2,1)
+monde = Environnement(100,100)  
+print("\n")
+monde.afficherEnv()
+print("\n")
+print("Robot1 initialisé dans la case",robot1.getPos(),"\n")
+print("Robot2 initialisé dans la case",robot1.getPos(),"\n")
+print("Obstacle occupe ces cases :",o.listePoints(),"\n")
+
+
+#Test Methodes ENV
+print("------------Add Robot & Update du monde------------\n")
+monde.add(robot2)
+print("\n")
+print("le robot se trouve en ",robot2.getPos()," et est dirigé vers ",robot2.getDirr())
+for i in range(0,3):
+    robot2.avancer()
+    monde.update(robot2)
+print("\nLe Robot tourne vers la droite\n")
+robot2.tourner('d')
+monde.update(robot2)
+
+for i in range(0,5):
+    robot2.avancer()
+    monde.update(robot2)
+
+#DETECTER
+
+
+#OBSTACLE
+print("------------LISTE DE POINTS DU ROBOT------------\n")
+o = Obstacle(0,0,2,4)
+print(o.listePoints())
+
+#ROTATION
+print("------------ROTATION DU ROBOT------------\n")
+robotTest = Robot(2,2,90)
+print("direction robot: ",robotTest.getDirr())
+print("position robot: ",robotTest.getPos())
+robotTest.rotation(-45)
+print("direction robot: ",robotTest.getDirr())
+
+#ANGLE VECTEUR
+print("------------ANGLE DU VECTEUR------------\n")
+print("angle du vecteur (1,1): ",angleVecteur((1,1)))
+
+#DEPLACEMENT ROBOT
+print("------------DEPLACEMENT------------\n")
+print("direction robot: ",robotTest.getDirr())
+print("position robot: ",robotTest.getPos())
+robotTest.deplacement((1,-1))
+print("angle du vecteur: ",angleVecteur((1,-1)))
+print("apres deplacement:")
+print("dir: ",robotTest.getDirr())
+print("pos: ",robotTest.getPos())
+
+
+
