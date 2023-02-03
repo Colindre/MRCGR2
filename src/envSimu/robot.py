4304d959e9eda548c05a405ac66c8f09d3e96891
LOUNICI <28600825@ppti-14-305-13.ufr-info-p6.jussieu.fr>
2023-02-03 11:09:49 +0100

    correction du code de ensPointsRobots

--- src/envSimu/robot.py
+++ src/envSimu/robot.py
@@ -2,19 +2,20 @@ import math
 
 class Robot:
 
+
 	def __init__(self,posx, posy, dirr,rayon):
 		self.posx = posx					#position du robot sur l'axe des abscisse 
 		self.posy = posy					#position du robot sur l'axe des abscisse 
 		self.dirr  = dirr%360				#direction du robot (angle en degre)
 		self.rayon=rayon
-	
+		
 
 	def getPos(self):						#retourne la position
 		return (self.posx, self.posy)
-		
+			
 	def getDirr(self):						#retourne la direction
 		return self.dirr
-		
+			
 	def getRayon(self):
 		return self.rayon
 
@@ -27,39 +28,20 @@ class Robot:
 		self.posx = (self.posx + vectX)
 		self.posy = (self.posy + vectY)
 
-		
+			
 	def ensPointsRobots(self):  #fonction pas correcte, a modifier
 		ens=set()
-		for i in range(self.posx, self.posx + self.rayon + 1):
-			for j in range(self.posy, self.posy + self.rayon + 1):
-				ens.add((i,j))
-		ensN=set()
-		for k in range(self.posx - self.rayon + 1,self.posx):
-			for l in range(self.posy - self.rayon + 1,self.posy):
-				ensN.add((k,l))
-		return ens.union(ensN)
-				
-	def detecter(self, ensPoints):					#verifie si le robot detecte un objet devant lui. tabPoints est un tableau de tuple
-	                                                        		#regarde si position du robot est dans le tableau de points
-		if (self.getPos() in ensPoints):
-			print("Robot est sur un obstacle.")
-			return True
-			
-			
-		if(self.dirr==1):											#regarde si le prochain deplacement vers l'avant resultera en une collision
-			testPos = (self.posx, self.posy+1)
-		elif(self.dirr==2):
-			testPos = (self.posx+1, self.posy)
-		elif(self.dirr==3):
-			testPos = (self.posx, self.posy -1)
-		else:
-			testPos = (self.posx-1, self.posy)
-		
-		if (testPos in ensPoints):	
-			print("Obstacle devant le robot.")
-			return True
-		print("pas d'obstacle devant")
-		return False
+		a=self.posx
+		b=self.posy
+		for i in range(self.posx-self.rayon, self.posx + self.rayon):
+			for j in range(self.posy-self.rayon, self.posy + self.rayon):
+				if(((i - a)**2 + (j - b)**2 )<= self.rayon**2):
+					ens.add((i,j))
+		return ens
+					
+	#def collision(self, ensPoints):					#verifie si le robot detecte un objet devant lui. ensPoints est un ensemble de tuple
+			                                                    		#regarde si position du robot est dans l'ensemble de points
+
 
 	def rotation(self, angle):						#fait tourner le robot (angle positif pour tourner a gauche, negatif pour a droite)
 		angle     = math.radians(angle%360)
@@ -76,15 +58,21 @@ class Robot:
 		else:
 			self.dirr = math.degrees(math.acos(x))
 
-def angleVecteur(vecteur):							#calcul l'angle positif du vecteur (par rapport a l'axe des abscisse)
-	x1, y1     = vecteur
-	x2, y2     = 1, 0
-	norme1     = math.sqrt(x1**2 + y1**2)
-	norme2     = math.sqrt(x2**2 + y2**2)
-	scalaire   = x1*x2 + y1*y2
-	angle      = math.degrees(math.acos(scalaire / (norme1*norme2)))
-	if(y1<0):										#permet de calculer l'angle positif
-		return 360-angle
-	else:
-		return angle
+	def angleVecteur(vecteur):							#calcul l'angle positif du vecteur (par rapport a l'axe des abscisse)
+		x1, y1     = vecteur
+		x2, y2     = 1, 0
+		norme1     = math.sqrt(x1**2 + y1**2)
+		norme2     = math.sqrt(x2**2 + y2**2)
+		scalaire   = x1*x2 + y1*y2
+		angle      = math.degrees(math.acos(scalaire / (norme1*norme2)))
+		if(y1<0):										#permet de calculer l'angle positif
+			return 360-angle
+		else:
+			return angle
+			
+			
+#test
+r1=Robot(2,2,90,3)
+print("r1,le robot de rayon",r1	.rayon)
+print("r1 est sur les points:",r1.ensPointsRobots())
 
