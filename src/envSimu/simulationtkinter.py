006c6bc18b3addaf68ecdcc65ab56ed96d628be6
Remi <rajaratnam.remi@gmail.com>
2023-02-19 22:54:32 +0100

    correction erreur sur le label(oublie de label.pack())

--- src/envSimu/simulationtkinter.py
+++ src/envSimu/simulationtkinter.py
@@ -17,6 +17,7 @@ class Simulationtkinter(tk.Tk):
         self.robot_canv = self.canvas.create_oval(self.robot.posx-self.robot.rayon,self.robot.posy-self.robot.rayon, self.robot.posx+self.robot.rayon, self.robot.posy+self.robot.rayon, fill='red')
         self.line = self.canvas.create_line(self.robot.posx,self.robot.posy,self.robot.posx+math.cos(math.radians(self.robot.dirr))*self.robot.rayon,self.robot.posx+math.sin(math.radians(self.robot.dirr))*self.robot.rayon, arrow="last")
         self.label = tk.Label( text="Vitesse gauche:         Vitesse droite:0\nAngle: 0\nPosition: (0, 0)")
+        self.label.pack()
         for i in self.environnement.ensObstacle:
             obstacle_canv=self.canvas.create_oval(i.posx-i.rayon,i.posy-i.rayon,i.posx+i.rayon,i.posy+i.rayon, fill=i.color)
         self.canvas.pack()
@@ -34,7 +35,7 @@ class Simulationtkinter(tk.Tk):
         self.quit_button.pack()
       
 
-#FONCTION LANCER SIMULATION
+#FONCTION LANCER SIMULATION 
 
     def update_robot(self):
         self.environnement.update()
