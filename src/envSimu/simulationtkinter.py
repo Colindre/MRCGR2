f8c504893102bbf5f84578aa49548d9449122257
Remi <rajaratnam.remi@gmail.com>
2023-02-19 18:28:34 +0100

    suppression bouttons et modification methode update_robot et loop

--- src/envSimu/simulationtkinter.py
+++ src/envSimu/simulationtkinter.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import math
 import random
+import threading
 from robot import Robot, angleVecteur
 from environnement import Obstacle, Environnement
 
@@ -9,25 +10,18 @@ class Simulationtkinter(tk.Tk):
         tk.Tk.__init__(self)
 #OBJETS
         self.environnement = environnement
-        self.temps=50
         self.bool = False
         self.robot = self.environnement.robot
 #CANVAS TKINTER
         self.canvas = tk.Canvas(self, bg='white', width=700, height=500)
-        self.canvas.pack()
         self.robot_canv = self.canvas.create_oval(self.robot.posx-self.robot.rayon,self.robot.posy-self.robot.rayon, self.robot.posx+self.robot.rayon, self.robot.posy+self.robot.rayon, fill='red')
         self.line = self.canvas.create_line(self.robot.posx,self.robot.posy,self.robot.posx+math.cos(math.radians(self.robot.dirr))*self.robot.rayon,self.robot.posx+math.sin(math.radians(self.robot.dirr))*self.robot.rayon, arrow="last")
         self.label = tk.Label( text="Vitesse gauche:         Vitesse droite:0\nAngle: 0\nPosition: (0, 0)")
-        self.label.pack()
-
         for i in self.environnement.ensObstacle:
             obstacle_canv=self.canvas.create_oval(i.posx-i.rayon,i.posy-i.rayon,i.posx+i.rayon,i.posy+i.rayon, fill=i.color)
+        self.canvas.pack()
 
 #BOUTTONS TKINTER
-        self.start_button = tk.Button(self, text='Lance Simulation', command=self.start)
-        self.start_button.pack()
-        self.stop_button = tk.Button(self, text='Stop Simulation', command=self.stop, state='disabled')
-        self.stop_button.pack()
         self.PlusVg = tk.Button(self, text='+ Vd', command=self.robot.augDPSd)
         self.PlusVg.pack(side='right')
         self.DimVg = tk.Button(self, text='- Vd', command=self.robot.dimDPSd)
@@ -36,42 +30,19 @@ class Simulationtkinter(tk.Tk):
         self.PlusVd.pack(side='left')
         self.DimVd = tk.Button(self, text='- Vg', command=self.robot.dimDPSg)
         self.DimVd.pack(side='left')
-        """self.quit_button = tk.Button(self, text='Quitte Simulation', command=self.quit)
-        self.quit_button.pack()"""
         self.quit_button = tk.Button(self, text="Quitter Simulation", command=self.destroy)
         self.quit_button.pack()
-    """def update_label(self, velocity, angle, position):
-        self.label.config(text=f"Vitesse: {velocity}\nAngle: {angle}\nPosition: {position}")"""
-
-
+      
 
 #FONCTION LANCER SIMULATION
-    def start(self):
-        self.bool = True
-        self.start_button['state'] = 'disabled'
-        self.stop_button['state'] = 'normal'
-        self.step()
-
-
-    def stop(self):
-        self.bool = False
-        self.start_button['state'] = 'normal'
-        self.stop_button['state'] = 'disabled'
-
-
-    def step(self):
-        if self.bool:
-            self.environnement.update()
-            self.update_robot()
-            print("direction",self.robot.dirr)
-            self.after(20, self.step)
 
     def update_robot(self):
-        #x1, y1, x2, y2 = self.canvas.coords(self.robot_canv)
+        self.environnement.update()
         self.canvas.coords(self.robot_canv,self.robot.posx-self.robot.rayon,self.robot.posy-self.robot.rayon,self.robot.posx+self.robot.rayon, self.robot.posy+self.robot.rayon)
         self.canvas.coords(self.line,self.robot.posx,self.robot.posy,self.robot.posx+math.cos(math.radians(self.robot.dirr))*self.robot.rayon,self.robot.posy+math.sin(math.radians(self.robot.dirr))*self.robot.rayon)
         self.label.config(text=f"Vitesse gauche: {self.robot.dpsG}          Vitesse droite: {self.robot.dpsD} \nAngle: {self.robot.dirr}\nPosition: {self.robot.getPos()}")
+        self.canvas.after(20, self.update_robot)
 
     def loop(self):
-        self.step
+        self.update_robot()   
         self.mainloop()
