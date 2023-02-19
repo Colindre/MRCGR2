732a4c2e4f2a7338f318ca4c89a5a44a35decc08
VILA Rodrigo <r94vila@gmail.com>
2023-02-19 14:37:16 +0100

    premiere version du controleur

--- src/envSimu/simulation.py
+++ src/envSimu/simulation.py
@@ -1,15 +1,30 @@
-from robot import Robot, angleVecteur
+from robot import Robot
 from environnement import Obstacle, Environnement
 from simulationtkinter import Simulationtkinter
 from time import sleep
-import math
-import random
 
-class Simulationtest:
 
-	def __init__(self,environnement,robot):
-		self.robot = robot
-		self.environnement = environnement
-	def updatesimulationtest(self):
-		self.simulationtkinter.start()
+def simulation(simulation, affichage, ia):
+
+    while True:
+        simulation.update()
+        if affichage != None:
+            affi.loop()
+        if ia != None:
+            ia.update()
+        sleep(0.5)
+
+env= Environnement(700, 500)
+rbt = Robot(250,250,90,50,3)
+#rbt.dpsD = 180
+obs1 = Obstacle(30,230,50,'black')
+obs2 = Obstacle(320,70,30,'yellow')
+
+env.addObstacle(obs1)
+env.addObstacle(obs2)
+env.add(rbt)
+affi=Simulationtkinter(env)
+#a.mainloop()
+simulation(env, affi, None)
+
 
