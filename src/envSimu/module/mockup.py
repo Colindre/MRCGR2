c0fb5c6e5a9d2fe298df89322be30795b2abe326
VILA Rodrigo <r94vila@gmail.com>
2023-03-08 14:35:15 +0100

    correction (ajout de pass)

--- src/envSimu/module/mockup.py
+++ src/envSimu/module/mockup.py
@@ -55,12 +55,13 @@ class Robot2IN013:
 
     def stop(self):
         """ Arrete le robot """
+        pass
 
     def get_image(self):
-
+        pass
 
     def get_images(self):
-
+        pass
   
     def set_motor_dps(self, port, dps):
         """
@@ -68,14 +69,14 @@ class Robot2IN013:
         :port: une constante moteur,  MOTOR_LEFT ou MOTOR_RIGHT (ou les deux MOTOR_LEFT+MOTOR_RIGHT).
         :dps: la vitesse cible en nombre de degres par seconde
         """
-
+        pass
 
     def get_motor_position(self):
         """
         Lit les etats des moteurs en degre.
         :return: couple du  degre de rotation des moteurs
         """
-
+        pass
    
     def offset_motor_encoder(self, port, offset):
         """
@@ -86,7 +87,7 @@ class Robot2IN013:
         :offset: l'offset de decalage en degre.
         Zero the encoder by offsetting it by the current position
         """
-
+        pass
     def get_distance(self):
         """
         Lit le capteur de distance (en mm).
@@ -94,23 +95,23 @@ class Robot2IN013:
             1. L'intervalle est de **5-8,000** millimeters.
             2. Lorsque la valeur est en dehors de l'intervalle, le retour est **8190**.
         """
-
+        pass
 
     def servo_rotate(self,position):
         """
         Tourne le servo a l'angle en parametre.
         :param int position: Angle de rotation, de **0** a **180** degres, 90 pour le milieu.
         """
-
+        pass
 
     def start_recording(self):
+        pass
 
     def _stop_recording(self):
-
+        pass
         
     def _start_recording(self):
-
-            print("Camera not found",e)
+        pass
 
     def __getattr__(self,attr):
         """ Méthodes héritées de GPG : 
@@ -122,3 +123,4 @@ class Robot2IN013:
             :green:  composante verte (0-255)
             :blue: composante bleu (0-255)
         """
+        pass
