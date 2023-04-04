8144d32effe7f7d7535dfb8ed787a1846f28c645
Remi <rajaratnam.remi@gmail.com>
2023-04-04 19:59:37 +0200

    corrections légères

--- src/envSimu/simulation.py
+++ src/envSimu/simulation.py
@@ -1,9 +1,10 @@
 from module.robot import Robot
+from module.mockup import Robot2IN013
 from module.environnement import Obstacle, Environnement
 from module.simulationtkinter import Simulationtkinter
 from time import sleep
 import threading
-from module.proxy import proxy_virtuel
+from module.proxy import proxy_virtuel, proxy_physique
 from module.ia import *
 
 
@@ -21,6 +22,8 @@ def simulation(simulation, affichage):
 #ROBOT
 rbt = Robot(250,250,180,50,100)
 
+rbtreel = Robot2IN013()
+
 #OBSTACLE
 obs1 = Obstacle(30,230,50,'black')
 obs2 = Obstacle(320,70,30,'yellow')
@@ -33,8 +36,9 @@ env.add(rbt)
 
 #PROXY
 rbt_simu = proxy_virtuel(env)
+rbt_reel = proxy_physique(rbtreel)
 
-#ACTION
+#ACTION VIRTUELLE
 dist50 = ParcourirDistance(rbt_simu,50,50)
 stop = Arrete(rbt_simu)
 demitourD = TournerDroiteAngle(rbt_simu,180,50)
@@ -49,8 +53,12 @@ ialoop = IAloop(rbt_simu, dist50)
 
 iaifte = IAifte(rbt_simu, ialoop, IAloop(rbt_simu, tourneG90), obstacle_proche)
 
+#ACTION REEL
+dist50reel = ParcourirDistance(rbt_reel,50,50)
+tourneD90reel = TournerDroiteAngle(rbt_reel,50,50)
+
 #IA
-rr = IA(rbt_simu,carre)
+rr = IA(rbt_simu,tourneD90reel)
 env.addIA(rr)
 
 
